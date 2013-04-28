bf9ec8ce7841d98a38533fff062a9c822ed7d24e
Samir Talwar <samir@noodlesandwich.com>
2013-04-28 19:47:33 +0100

    The application server should be a singleton.

--- src/main/java/com/noodlesandwich/quacker/server/ApplicationServer.java
+++ src/main/java/com/noodlesandwich/quacker/server/ApplicationServer.java
@@ -1,9 +1,11 @@
 package com.noodlesandwich.quacker.server;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import com.noodlesandwich.quacker.user.User;
 import com.noodlesandwich.quacker.user.Users;
 
+@Singleton
 public class ApplicationServer implements Server {
     private final Users users;
 
