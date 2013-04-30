c0969a238e12c8ea73997136765bb7bdf5fd0892
Samir Talwar <samir@noodlesandwich.com>
2013-04-30 21:46:24 +0100

    You can have lotsa messages.

--- src/main/java/com/noodlesandwich/quacker/user/InMemoryUser.java
+++ src/main/java/com/noodlesandwich/quacker/user/InMemoryUser.java
@@ -1,18 +1,24 @@
 package com.noodlesandwich.quacker.user;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 import com.noodlesandwich.quacker.message.Message;
 import com.noodlesandwich.quacker.ui.MessageRenderer;
 
 public class InMemoryUser implements User {
-    private Message message;
+    private List<Message> messages = new ArrayList<>();
 
     @Override
     public void publish(Message message) {
-        this.message = message;
+        this.messages.add(message);
     }
 
     @Override
     public void renderTimelineTo(MessageRenderer renderer) {
-        message.renderTo(renderer);
+        ListIterator<Message> messageIterator = messages.listIterator(messages.size());
+        while (messageIterator.hasPrevious()) {
+            messageIterator.previous().renderTo(renderer);
+        }
     }
 }
--- src/test/java/com/noodlesandwich/quacker/user/InMemoryUserTest.java
+++ src/test/java/com/noodlesandwich/quacker/user/InMemoryUserTest.java
@@ -4,6 +4,7 @@ import com.noodlesandwich.quacker.message.Message;
 import com.noodlesandwich.quacker.ui.MessageRenderer;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.Sequence;
 import org.junit.Test;
 
 public class InMemoryUserTest {
@@ -17,6 +18,25 @@ public class InMemoryUserTest {
 
         context.checking(new Expectations() {{
             oneOf(renderer).render("Beep beep.");
+        }});
+
+        user.renderTimelineTo(renderer);
+
+        context.assertIsSatisfied();
+    }
+
+    @Test public void
+    messages_are_rendered_in_reverse_chronological_order() {
+        User user = new InMemoryUser();
+        user.publish(new Message("One"));
+        user.publish(new Message("Two"));
+        user.publish(new Message("Three"));
+
+        context.checking(new Expectations() {{
+            Sequence messages = context.sequence("messages");
+            oneOf(renderer).render("Three"); inSequence(messages);
+            oneOf(renderer).render("Two"); inSequence(messages);
+            oneOf(renderer).render("One"); inSequence(messages);
         }});
 
         user.renderTimelineTo(renderer);
