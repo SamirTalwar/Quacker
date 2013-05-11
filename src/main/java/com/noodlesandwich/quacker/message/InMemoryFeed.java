18fc06e7ca708562855e0c07a8d245ea18020278
Samir Talwar <samir@noodlesandwich.com>
2013-05-11 17:15:11 +0100

    Render messages from a single followee.

--- src/main/java/com/noodlesandwich/quacker/message/InMemoryFeed.java
+++ src/main/java/com/noodlesandwich/quacker/message/InMemoryFeed.java
@@ -1,14 +1,28 @@
 package com.noodlesandwich.quacker.message;
 
 import com.noodlesandwich.quacker.ui.FeedRenderer;
+import com.noodlesandwich.quacker.ui.TimelineRenderer;
 import com.noodlesandwich.quacker.user.Profile;
 
 public class InMemoryFeed implements Feed {
+    private Profile followee;
+
     @Override
     public void follow(Profile profile) {
+        this.followee = profile;
     }
 
     @Override
     public void renderTo(FeedRenderer renderer) {
+        if (followee == null) {
+            return;
+        }
+
+        followee.renderTimelineTo(new TimelineRenderer() {
+            @Override
+            public void render(Message message) {
+                renderer.render(message);
+            }
+        });
     }
 }
--- src/test/java/com/noodlesandwich/quacker/message/InMemoryFeedTest.java
+++ src/test/java/com/noodlesandwich/quacker/message/InMemoryFeedTest.java
@@ -1,17 +1,26 @@
 package com.noodlesandwich.quacker.message;
 
+import java.util.ArrayList;
+import java.util.List;
 import com.noodlesandwich.quacker.ui.FeedRenderer;
+import com.noodlesandwich.quacker.ui.TimelineRenderer;
+import com.noodlesandwich.quacker.user.Profile;
+import org.hamcrest.Description;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.Sequence;
+import org.jmock.api.Action;
+import org.jmock.api.Invocation;
 import org.junit.Test;
 
 public class InMemoryFeedTest {
     private final Mockery context = new Mockery();
     private final Feed feed = new InMemoryFeed();
 
+    private final FeedRenderer renderer = context.mock(FeedRenderer.class);
+
     @Test public void
     an_empty_feed_has_no_output() {
-        FeedRenderer renderer = context.mock(FeedRenderer.class);
         context.checking(new Expectations() {{
             never(renderer);
         }});
@@ -19,5 +28,55 @@ public class InMemoryFeedTest {
         feed.renderTo(renderer);
 
         context.assertIsSatisfied();
+    }
+
+    @Test public void
+    following_a_user_results_in_a_feed_containing_quacks_from_that_user() {
+        Profile profile = context.mock(Profile.class);
+        feed.follow(profile);
+
+        Message one = new Message("One.");
+        Message two = new Message("Two.");
+        Message three = new Message("Three.");
+
+        List<Message> messages = new ArrayList<>();
+        messages.add(three);
+        messages.add(two);
+        messages.add(one);
+
+        context.checking(new Expectations() {{
+            allowing(profile).renderTimelineTo(with(any(TimelineRenderer.class))); will(new RenderMessages(messages));
+
+            Sequence messages = context.sequence("messages");
+            oneOf(renderer).render(three); inSequence(messages);
+            oneOf(renderer).render(two); inSequence(messages);
+            oneOf(renderer).render(one); inSequence(messages);
+        }});
+        feed.renderTo(renderer);
+
+        context.assertIsSatisfied();
+    }
+
+    private static class RenderMessages implements Action {
+        private final List<Message> messages;
+
+        public RenderMessages(List<Message> messages) {
+            this.messages = messages;
+        }
+
+        @Override
+        public Object invoke(Invocation invocation) throws Throwable {
+            TimelineRenderer timelineRenderer = (TimelineRenderer) invocation.getParameter(0);
+            for (Message message : messages) {
+                timelineRenderer.render(message);
+            }
+            return null;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("render the messages")
+                       .appendValueList("", ", ", "", messages);
+        }
     }
 }
